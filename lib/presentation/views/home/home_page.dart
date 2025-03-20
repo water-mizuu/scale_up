17b4fb9eee6be3a7c6e62a1afeaaad1e9991f4aa
XLokbu <siggayovincent@gmail.com>
2025-03-20 23:11:36 +0800

    added course tiles and search bar in the home screen

--- lib/presentation/views/home/home_page.dart
+++ lib/presentation/views/home/home_page.dart
@@ -17,23 +17,163 @@ class HomePage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [Text('Daily Practice')],
             ),
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Icon(Icons.search, size: 18,),
+                ),
+                border: OutlineInputBorder(),
+                hintText: "Search",
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(7),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Welcome!",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "Let us do something rawr",
+                        style: TextStyle(
+                            fontSize: 14,
+                          ),
+                      ),
+                    ],
+                  ),
+                  Image.asset(
+                    "assets/illustrations/topic.png",
+                    height: 80,
+                    fit: BoxFit.cover,
+                  )
+                ],
+              ),
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
+                Text(
+                  "Ongoing Projects",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                InkWell(
+                  onTap: (){
+                    print("Ito lang muna laman mb!");
+                  },
+                  child: Text(
+                  "See all",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Color.fromRGBO(0, 0, 0, 0.5),
+                  ),
+                ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
                 Expanded(
                   child: Card.filled(
                     child: Padding(
-                      padding: const EdgeInsets.all(16.0),
+                      padding: const EdgeInsets.all(8.0),
                       child: Column(
-                        children: [],
+                        children: [
+                          Row(
+                            spacing: 10,
+                            children: [
+                              CourseTile(label: "DISTANCE", icon: Icons.social_distance, progressBarValue: 0.2),
+                              CourseTile(label: "TEMPERATURE", icon: Icons.bathtub, progressBarValue: 0.7),
+                            ],
+                          )
+                         
+                        ],
                       ),
                     ),
                   ),
                 ),
               ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CourseTile extends StatelessWidget {
+  const CourseTile({
+    required this.label,
+    required this.icon,
+    required this.progressBarValue,
+    super.key
+  });
+
+  final IconData icon;
+  final String label;
+  final double progressBarValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(7),
+        border: Border.all(
+          color: Colors.black,
+          width: 1.0,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              spacing: 8,
+              children: [
+                Icon(icon),
+                Text(
+                  label,
+                  style: TextStyle(
+                    fontSize: 13.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Progress",
+              style: TextStyle(
+                fontSize: 12.0,
+              ),
+            ),
+            SizedBox(height: 5,),
+            LinearProgressIndicator(
+              value: progressBarValue,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
             )
           ],
         ),
       ),
+    ),
     );
   }
 }
@@ -73,7 +213,7 @@ class UserBar extends StatelessWidget {
           style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.white)),
           icon: Icon(Icons.notifications),
         ),
-        CircleAvatar(radius: 22.0, child: Text('U'))
+        CircleAvatar(radius: 22.0, child: Text('X'))
       ],
     );
   }
